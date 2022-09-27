f010e81442806022f66cc30aba4aef6f56fc1a01
jeongseongeop <wjdtjddjq1@gmail.com>
2022-09-27 22:31:27 +0900

    220927

--- Example/urls.py
+++ Example/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-
 from . import views
+from .views import index, detail
 
 urlpatterns = [
-    path('',views.index),
-    path('<int:question_id>/', views.detail)
+    path('',views.index, name= index),
+    path('<int:question_id>/', views.detail, name=detail)
 ]
